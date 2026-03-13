682e0e0ad5b3c272548f0087281a169374966e3a
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2026-03-13 17:02:53 +0800

    fix: about script dont do it twice

--- js/aboutscript.js
+++ js/aboutscript.js
@@ -154,6 +154,4 @@ modaloverlaylife.addEventListener("click", function(event){
     }
 })
 
-const footeryear = document.getElementById('footeryear');
-footeryear.textContent = new Date().getFullYear();
 
